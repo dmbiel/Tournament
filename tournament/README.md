b89dffa42d5335f2c926066f9b69c020ad230000
Patryk Pająk <pionq1992@gmail.com>
2017-06-07 11:27:48 +0200

    entities

--- tournament/README.md
+++ tournament/README.md
@@ -1,4 +1,4 @@
 tournament
 ==========
 
-A Symfony project created on June 6, 2017, 12:22 pm.
+A Symfony project created on June 6, 2017, 1:50 pm.
---
+++ tournament/src/AppBundle/Entity/MatchDate.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MatchDate
+ *
+ * @ORM\Table(name="match_date")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MatchDateRepository")
+ */
+class MatchDate
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="date", type="string", length=255)
+     */
+    private $date;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="status", type="boolean")
+     */
+    private $status;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param string $date
+     * @return MatchDate
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return string 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set status
+     *
+     * @param boolean $status
+     * @return MatchDate
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return boolean 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
